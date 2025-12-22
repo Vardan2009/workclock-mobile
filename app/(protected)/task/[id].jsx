4b23551ac606573dc40090c3eca05e717c63569c
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-12-23 00:51:14 +0400

    Task notes

--- app/(protected)/task/[id].jsx
+++ app/(protected)/task/[id].jsx
@@ -1,6 +1,7 @@
 import ThemedText from "../../../src/components/styled/themedText";
 import ThemedView from "../../../src/components/styled/themedView";
 import ThemedButton from "../../../src/components/styled/themedButton";
+import ThemedInput from "../../../src/components/styled/themedInput";
 
 import { useStyles } from "../../../src/globalStyle";
 
@@ -14,24 +15,49 @@ import { router } from "expo-router";
 
 import { ScrollView } from "react-native";
 
+import { useEffect, useState, useRef } from "react";
+
 import { useTheme } from "../../../src/themeCtx";
+
+import { Feather } from "@expo/vector-icons";
+import { api } from "../../../src/api/client";
 
 export default function TaskDetail() {
     const styles = useStyles();
 
     const params = useLocalSearchParams();
 
+    const [unsaved, setUnsaved] = useState(false);
+    const [saving, setSaving] = useState(false);
+    const debounceTimeout = useRef(null);
+
     const { tasks } = useTasks();
     const { theme } = useTheme();
 
     const task = tasks.find((t) => t.id == params.id);
 
+    const [notes, setNotes] = useState(task.notes);
+
+    const saveNotes = (t) => {
+        setNotes(t);
+        setUnsaved(true);
+        setSaving(false);
+
+        if (debounceTimeout.current) clearTimeout(debounceTimeout.current);
+
+        debounceTimeout.current = setTimeout(async () => {
+            setSaving(true);
+            await api.patch(`/tasks/${task.id}/note`, { note: t });
+            setUnsaved(false);
+            setSaving(false);
+        }, 300);
+    };
+
     return (
-        <ThemedView style={styles.page}>
+        <ScrollView style={styles.page}>
             <ThemedText style={styles.title}>
                 {task.icon} {task.title}
             </ThemedText>
-
             <ThemedText>
                 Avg. estimate: {formatSecondsToHMS(task.getAvgEstTime())}
             </ThemedText>
@@ -47,13 +73,41 @@ export default function TaskDetail() {
                 Total instances: {task.taskInstances.length}
             </ThemedText>
 
-            <ThemedText style={[styles.subtitle, { marginVertical: 30 }]}>
+            <ThemedText style={[styles.subtitle, { marginTop: 30 }]}>
+                Task Notes{" "}
+                {unsaved && (
+                    <ThemedText style={{ color: theme.danger }}>*</ThemedText>
+                )}{" "}
+                {saving && (
+                    <ThemedText style={{ marginLeft: 10 }}>
+                        Saving...
+                    </ThemedText>
+                )}
+            </ThemedText>
+
+            <ThemedInput
+                value={notes}
+                onChangeText={saveNotes}
+                multiline={true}
+                placeholder="Your notes here..."
+                style={{ height: 300, padding: 10, fontSize: 16 }}
+            />
+
+            <ThemedText style={[styles.subtitle, { marginTop: 30 }]}>
                 Task Instances
             </ThemedText>
 
-            <ScrollView>
+            <ScrollView style={{ height: 500 }}>
                 {task.taskInstances.toReversed().map((inst, i) => (
-                    <ThemedView key={i} style={{ marginVertical: 10 }}>
+                    <ThemedView
+                        key={i}
+                        style={{
+                            marginVertical: 10,
+                            backgroundColor: theme.bgAlt,
+                            padding: 5,
+                            borderRadius: 5,
+                        }}
+                    >
                         <ThemedText
                             style={{
                                 fontFamily: "BrandFont-Bold",
@@ -87,13 +141,12 @@ export default function TaskDetail() {
                     </ThemedView>
                 ))}
             </ScrollView>
-
             <ThemedButton
                 style={{ marginTop: 30 }}
                 onPress={() => router.back()}
             >
-                &lt; Back
+                <Feather style name="arrow-left" size={16} /> Back
             </ThemedButton>
-        </ThemedView>
+        </ScrollView>
     );
 }
--- src/util.js
+++ src/util.js
@@ -8,9 +8,9 @@ export function formatSecondsToHMS(totalSeconds) {
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
-    const hoursStr = hours.toString().padStart(2, "0");
-    const minutesStr = minutes.toString().padStart(2, "0");
-    const secondsStr = seconds.toString().padStart(2, "0");
+    const hoursStr = hours.toFixed(0).padStart(2, "0");
+    const minutesStr = minutes.toFixed(0).padStart(2, "0");
+    const secondsStr = seconds.toFixed(0).padStart(2, "0");
 
     return `${sign}${hoursStr}:${minutesStr}:${secondsStr}`;
 }
