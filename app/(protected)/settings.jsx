b41ee2e886409f19ee6f6a2bbdeb9a818c86defc
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-12-23 22:37:26 +0400

    Updated version

--- app/(protected)/settings.jsx
+++ app/(protected)/settings.jsx
@@ -32,7 +32,7 @@ export default function Settings() {
             </ThemedButton>
 
             <ThemedText style={{ marginTop: 100, opacity: 0.5 }}>
-                WorkClock version v0.2.0
+                WorkClock version v0.2.1
             </ThemedText>
 
             <ThemedPressable
